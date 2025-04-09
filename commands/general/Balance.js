505144343be71e6df2c3849a0672c2308b8c2990
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-04-09 17:31:37 +0530

    Update Balance.js

--- commands/general/Balance.js
+++ commands/general/Balance.js
@@ -1,31 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-
-const dataPath = path.join(__dirname, '../../data/messageLimits.json');
+const MessageLimit = require('../../models/MessageLimit');
 
 const NON_PREMIUM_LIMIT = 50;
 const PREMIUM_LIMIT = 15;
 
 module.exports = {
-  name: 'Balance',
+  name: 'messageLimits',
   description: 'Check how many messages you have left',
   premium: false,
   async execute(client, message, args, userStatus) {
     const userId = message.author.id;
 
-    let messageLimits;
-    try {
-      const fileData = fs.readFileSync(dataPath, 'utf8');
-      messageLimits = JSON.parse(fileData);
-    } catch (err) {
-      console.error('Error reading messageLimits.json:', err);
-      return message.reply('There was an error retrieving your message limits.');
-    }
-
-    const nonPremiumUsed = Object.values(userLimits.nonPremium || {}).reduce((sum, count) => sum + count, 0);
+    let userLimits = await MessageLimit.findOne({ userId });
+ 
+    const nonPremiumUsed = Array.from(userLimits.nonPremium.values()).reduce((sum, count) => sum + count, 0);
     const nonPremiumLeft = NON_PREMIUM_LIMIT - nonPremiumUsed;
 
-    const premiumUsed = Object.values(userLimits.premium || {}).reduce((sum, count) => sum + count, 0);
+    const premiumUsed = Array.from(userLimits.premium.values()).reduce((sum, count) => sum + count, 0);
     const premiumLeft = PREMIUM_LIMIT - premiumUsed;
 
     return message.reply(
